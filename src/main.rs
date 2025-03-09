fa873a170e74e402276587ca5166dcd5d4edfa6f
jasonandmonte <jason.monte55@gmail.com>
2025-03-09 14:24:50 -0700

    Add functions
    Refactor to place logic into functions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use nannou::prelude::*;
 
-const CIRCLE_SIZE: f32 = 36.0;
+const CIRCLE_SIZE: f32 = 32.0;
 const RING_SPACING: f32 = 40.0;
 
 struct Model {
@@ -29,48 +29,15 @@ fn model(_app: &App) -> Model {
     let (integer, modulus) = parse_args();
     // TODO: Add --help flag
 
-    
-    // TODO: Create a function
-    let num_rings = integer.div_ceil(modulus);
-
-    // Determine coordinates for each point
-    // FIXME: magic numbers
-    let ring_radius = CIRCLE_SIZE / 3.0 * (modulus as f32);
-    let ring_points  = modulus; 
-    let stride = 360 / ring_points;
-    let mut points = vec![];
-
-    let mut count: u32 = 0;
-    for nr in 0..num_rings {
-        let scale_factor = ring_radius + (nr as f32) * RING_SPACING;
-        // Map over an array of integers from 0 to 360 to represent the degrees in a circle.
-        for i in (0..360).step_by(stride as usize) {
-            let radian = deg_to_rad(i as f32);
-            // Get the sine of the radian to find the x co-ordinate of this point of the circle
-            // and multiply it by the ring radius.
-            let x = radian.sin() * scale_factor;
-            // Do the same with cosine to find the y co-ordinate.
-            let y = radian.cos() * scale_factor;
-            // Construct and return a point object with a color.
-            // (x,y)
-            points.push(Point {
-                x,
-                y,
-                label: count.to_string(),
-            });
-            
-            count += 1;
-        }
-    }
-
     Model {
         integer,
         modulus,
         result: integer % modulus,
-        points,
+        points: generate_points(integer, modulus),
     }
 }
 
+/// TODO:
 fn parse_args() -> (u32, u32) {
     let args: Vec<String> = env::args().collect();
 
@@ -98,41 +65,83 @@ fn parse_args() -> (u32, u32) {
     (integer, modulus)
 }
 
+///
+fn generate_points(integer: u32, modulus: u32) -> Vec<Point> {
+    // TODO: Create a function
+    let num_rings = integer.div_ceil(modulus);
+
+    // Determine coordinates for each point
+    // FIXME: magic numbers
+    let ring_radius = CIRCLE_SIZE / 3.0 * (modulus as f32);
+    let ring_points  = modulus;
+    let stride = 360 / ring_points;
+    let mut points = vec![];
+
+    let mut number: u32 = 0;
+    for nr in 0..num_rings {
+        let scale_factor = ring_radius + (nr as f32) * RING_SPACING;
+        // Map over an array of integers from 0 to 360 to represent the degrees in a circle.
+        for i in (0..360).step_by(stride as usize) {
+            let radian = deg_to_rad(i as f32);
+            // Get the sine of the radian to find the x co-ordinate of this point of the circle
+            // and multiply it by the ring radius.
+            let x = radian.sin() * scale_factor;
+            // Do the same with cosine to find the y co-ordinate.
+            let y = radian.cos() * scale_factor;
+            // Construct and return a point object with a color.
+            // (x,y)
+            points.push(Point {
+                x,
+                y,
+                label: number.to_string(),
+            });
+            
+            number += 1;
+        }
+    }
+    points
+}
+
 fn event(_app: &App, _model: &mut Model, _event: Event) {}
 
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     draw.background().color(WHITE);
-   
 
-    // TODO: draw_rings function
-    let time: f32 = app.time;
     // Show a point every second
-    let timed_draw = (time as usize).min(model.points.len());
+    let time = (app.time as usize).min(model.points.len());
 
+    draw_rings(&draw, model, time);
+    draw_points(&draw, model, time);
+
+    draw.to_frame(app, &frame).unwrap();
+}
+
+/// TODO: Function docs
+fn draw_rings(draw: &Draw, model: &Model, time: usize) {
     // FIXME: Account for ring levels
-    let ring_opacity = 1.0; //if timed_draw == points.len() { 1.0 } else { 0.0 };
+    let ring_opacity = 1.0; //if time == points.len() { 1.0 } else { 0.0 };
 
     // NOTE: nannou layers shapes based on when they are drawn.
     // To have rings display below the integer shapes they must be drawn first.
     // Draw largest first so it is the base layer
     for nr in (0..model.integer.div_ceil(model.modulus)).rev() {
-        draw.ellipse()
+        draw.ellipse()  
             .radius(CIRCLE_SIZE / 3.0 * (model.modulus as f32) + (nr as f32) * RING_SPACING)
             .no_fill()
             .stroke(rgba(0.0, 0.75, 1.0, ring_opacity))
             .stroke_weight(2.0);
     }
+}
 
-
-    // TODO: draw_points function
+/// TODO:
+fn draw_points(draw: &Draw, model: &Model, time: usize) {
     for (i, point) in model.points.iter().enumerate() {
-        if i >= timed_draw {
+        if i >= time {
             break;
         }
     
         let Point { x, y, label } = point;
-        println!("{}:({},{})", label, x, y);
     
         draw.ellipse()
             .w_h(CIRCLE_SIZE, CIRCLE_SIZE)
@@ -145,6 +154,4 @@ fn view(app: &App, model: &Model, frame: Frame) {
             .color(BLACK)
             .font_size(14);
     }
-
-    draw.to_frame(app, &frame).unwrap();
 }
